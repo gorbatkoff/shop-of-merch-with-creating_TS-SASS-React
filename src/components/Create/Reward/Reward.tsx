acc681a7bebaa92b1cd2a049d6281d485040f6a7
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-18 03:07:46 +0300

    Create Reward component

--- src/components/Create/Reward/Reward.tsx
+++ src/components/Create/Reward/Reward.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
+import Image from './RewardByType/Image';
+import Video from './RewardByType/Video';
+import Audio from './RewardByType/Audio';
+import File from './RewardByType/File';
+import Link from './RewardByType/Link';
+import Text from './RewardByType/Text';
+import Custom from './RewardByType/Custom';
 
-function Reward({}: Props) {
-  return (
-    <div>Reward</div>
-  )
+type Props = {
+    reward: string
+}
+
+const styles = {
+    margin: "1em 0",
+    fontWeight: "bold",
+    color: "#0034BB"
+}
+
+function Reward({ reward }: Props) {
+    return (
+        <div style={styles}>
+            {(reward === 'image') ? <Image /> :
+                    (reward === 'video') ? <Video /> :
+                        (reward === 'audio') ? <Audio /> :
+                            (reward === 'file') ? <File /> :
+                                (reward === 'link') ? <Link /> :
+                                    (reward === 'text') ? <Text /> :
+                                        (reward === 'custom') ? <Custom /> : <Image />}
+        </div>
+    )
 }
 
 export default Reward
