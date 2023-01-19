c93b73031b666e00e0121d3fd59eaec55a6d6232
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-19 23:40:02 +0300

    Add components and valid radio buttons

--- src/components/Create/Create.tsx
+++ src/components/Create/Create.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
-import Radio from '@mui/material/Radio';
-
 import { useForm, SubmitHandler } from "react-hook-form";
 
 import UserIcon from '../../Images/UserIcon.svg';
 
+import Radio from '@mui/material/Radio';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+
+import Title from '../UI/Title';
+import Reward from './Reward/Reward';
 import styles from './Create.module.scss';
 import MyDropzone from '../Dropzone/MyDropzone';
-import Reward from './Reward/Reward';
 
 type FormValues = {
   nameOfReward: string;
@@ -19,6 +25,7 @@ type FormValues = {
   isHaveDivisionByGender: boolean;
   note: string;
   isPrivate: boolean;
+  developer: boolean;
 };
 
 type Props = {}
@@ -33,26 +40,49 @@ function Create({ }: Props) {
 
       <MyDropzone />
 
-      <h3>Название вознаграждения</h3>
-      <input {...register("nameOfReward")} type="text" id="nameOfReward" />
+      <Title header="Название вознаграждения" title="Введите название вознаграждения отражающее то, что получит пользователь"/>
 
-      <h4>Цена в количестве приведённых пользователей</h4>
+      <input {...register("nameOfReward")} type="text" id="nameOfReward" placeholder='Худи с красивой надписью' />
+
+      <Title header="Цена в количестве приведённых пользователей" title="Введите цену исходя из того, что 1 приведённый пользователь = 1 монета"/>
+
 
       <div className={styles.referral}>
         <img src={UserIcon} alt="" />
-        <input {...register('price', { required: true, max: 1000000, min: 0 })} id="price" type="number" style={{ width: "20%" }} />
+        <input {...register('price', { required: true, max: 1000000, min: 0 })} id="price" type="number" style={{ width: "35%" }} placeholder='1 000' />
         <p>Максимум 1 000 000</p>
       </div>
 
-      <h3>Вид доставки</h3>
-      <Radio {...register("kindOfDelivery", { required: true })} value="virtual" id='kindOfDelivery' />
-      <label htmlFor="kindOfDelivery">Виртуальная доставка</label>
-      <Radio {...register("kindOfDelivery", { required: true })} value="offline" id='kindOfDelivery' />
-      <label htmlFor="kindOfDelivery">Оффлайн доставка</label>
+      <Title header="Вид доставки" title="Укажите тип доставки, где виртуальная — через интернет (видео или аудиозаписи), где оффлайн — доставка почтой до дома получателя."/>
 
-      <h3>Тип вознаграждения</h3>
+      {/* <div style={{ display: 'flex', alignItems: "center", justifyContent: "space-between", width: "100%" }}>
+        <div>
+          <Radio {...register("kindOfDelivery", { required: true })} value="virtual"  />
+          <input {...register("developer", { required: true })} type="radio" value="virtual" id='kindOfDelivery' />
+          <label htmlFor="kindOfDelivery">Виртуальная доставка</label>
+        </div>
+        <div>
+          <Radio {...register("kindOfDelivery", { required: true })} value="offline" id='kindOfDelivery' />
+          <input {...register("developer", { required: true })} type="radio" value="offline" id='kindOfDelivery' />
+          <label htmlFor="kindOfDelivery">Оффлайн доставка</label>
+        </div>
+      </div> */}
 
-      <select {...register("typeOfDelivery", { required: true })}>
+      <FormControl>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel {...register("isPrivate", { required: true })} value="female" control={<Radio />} label="Female" />
+          <FormControlLabel {...register("isPrivate", { required: true })} value="male" control={<Radio />} label="Male" />
+          <FormControlLabel {...register("isPrivate", { required: true })} value="other" control={<Radio />} label="Other" />
+        </RadioGroup>
+      </FormControl>
+
+      <Title header="Тип вознаграждения" title="Тип вознаграждения — это то, что получит пользователь после оплаты товара: ссылка, аудио, текст. Где кастомизированный — это товар (мерч) который отправляется до получателя (кроссовки, худи, толстовка, предмет)"/>
+
+      <select {...register("typeOfDelivery", { required: true })} className={styles.select}>
         <option value="image" key={1}>Изображение</option>
         <option value="video" key={2}>Видеофайл</option>
         <option value="audio" key={3}>Аудиофайл</option>
@@ -62,18 +92,17 @@ function Create({ }: Props) {
         <option value="custom" key={7}>Кастомизированный (обязательства)</option>
       </select>
 
-      <h3>Вознаграждение</h3>
-
-      <Reward reward={watch("typeOfDelivery")}/>
+      <Reward reward={watch("typeOfDelivery")} />
 
 
-      <h3>Примечание для пользователей</h3>
+      <Title header="Примечание для пользователя" title="Примечание — это сообщение, которое получит пользователь после покупки товара. Например благодарность или просьба сообщить адрес доставки. "/>
+
 
       <p className={styles.paragraph}>
         Сообщение, которое пользователь увидит после обмена накопленных пользователей на вознаграждения. Например, инструкцию как получить</p>
 
 
-      <textarea {...register("note")} className={styles.note}>
+      <textarea {...register("note")} className={styles.note} placeholder='Привет дорогой подписчик! Я очень благодарен тебе за твою помощь, скажи мне свой адрес доставки, чтоб я смог отправить тебе мой мерч :)'>
       </textarea>
 
       <br />
