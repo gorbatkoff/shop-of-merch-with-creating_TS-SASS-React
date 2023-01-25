b582d73e069003e9150662a5da435793267db2a9
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-25 05:33:51 +0300

    Init

--- src/components/TelegramAuthButton/TelegramWidget.tsx
+++ src/components/TelegramAuthButton/TelegramWidget.tsx
@@ -1,28 +1,33 @@
 import React, { useEffect } from 'react';
 
-const TelegramWidget: React.FC = () => {
-
-    function onTelegramAuth(){
-        console.log('Success!')
+declare global {
+    interface Window {
+      onTelegramAuth: (user: any) => void;
     }
+  }
 
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = 'https://telegram.org/js/telegram-widget.js?21';
-        script.setAttribute('data-telegram-login', 'subs_web_app_bot');
-        script.setAttribute('data-size', 'large');
-        script.setAttribute('data-radius', '20');
-        script.setAttribute('data-onauth', `${onTelegramAuth}`);
-        script.setAttribute('data-request-access', 'write');
-        document.body.appendChild(script);
+const TelegramAuth: React.FC = () => {
+  useEffect(() => {
+    // Добавляем скрипт для виджета авторизации Telegram
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-widget.js?21';
+    script.setAttribute('data-telegram-login', 'samplebot');
+    script.setAttribute('data-size', 'large');
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    script.setAttribute('data-request-access', 'write');
+    script.async = true;
+    document.body.appendChild(script);
+    
+    // Функция для обработки результата авторизации
+    window.onTelegramAuth = (user: any) => {
+        console.log('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+        // Здесь вы можете добавить дополнительную логику для аутентификации и сохранения информации о пользователе
+      };
+  }, []);
 
-        return () => {
-            document.body.removeChild(script);
-        }
-    }, []);
+  return <div>Кнопка авторизации через Telegram будет отображена здесь</div>
+};
 
-    return <div id="telegram-login"></div>;
-}
+export default TelegramAuth;
 
-export default TelegramWidget;
+
