53743d223184c98d6169ae25741807258a052ca0
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-25 05:41:18 +0300

    Init

--- src/components/TelegramAuthButton/TelegramWidget.tsx
+++ src/components/TelegramAuthButton/TelegramWidget.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 
+const botUsername = '5833515258:AAHm5gAkLr7XcD3JGiQdOxDRXky3sby6dUw';
+
 declare global {
     interface Window {
       onTelegramAuth: (user: any) => void;
@@ -11,7 +13,7 @@ const TelegramAuth: React.FC = () => {
     // Добавляем скрипт для виджета авторизации Telegram
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?21';
-    script.setAttribute('data-telegram-login', 'samplebot');
+    script.setAttribute('data-telegram-login', botUsername);
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-onauth', 'onTelegramAuth(user)');
     script.setAttribute('data-request-access', 'write');
