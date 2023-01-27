8f416eb264ceb340edb3e98880e2c3e93a760095
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-28 01:57:19 +0300

    Create Telegram widget and add sending data to API

--- src/components/TelegramAuthButton/TelegramWidget.tsx
+++ src/components/TelegramAuthButton/TelegramWidget.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect } from 'react';
 
 const botUsername = 'OhMySubsBot';
@@ -8,6 +9,18 @@ declare global {
     }
 }
 
+const API = 'https://1cde-31-173-85-60.eu.ngrok.io/auth/by_telegram/';
+
+const sendDataToApi = async (url: string) => {
+    try {
+        let request = await axios.get(API + url);
+
+        console.log(request);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const TelegramAuth: React.FC = () => {
     useEffect(() => {
         // Добавляем скрипт для виджета авторизации Telegram
@@ -15,22 +28,33 @@ const TelegramAuth: React.FC = () => {
         script.src = 'https://telegram.org/js/telegram-widget.js?21';
         script.setAttribute('data-telegram-login', botUsername);
         script.setAttribute('data-size', 'large');
-        script.setAttribute('data-userpic', 'false');
-        script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+        script.setAttribute('data-auth-url', 'https://17a1-95-174-105-255.eu.ngrok.io/');
+        // script.setAttribute('data-onauth', 'onTelegramAuth(user)');
         script.setAttribute('data-request-access', 'write');
         script.async = true;
         document.body.appendChild(script);
 
         // Функция для обработки результата авторизации
-        window.onTelegramAuth = (user: any) => {
-            console.log(user)
-            // console.log('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-            // window.location.href="./channel"
-        };
+        // window.onTelegramAuth = (user: any) => {
+            
+        //     console.log(user)
+        //     console.log('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+        //     window.location.href="./channel"
+        //     <script async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-login="samplebot" data-size="large" data-auth-url="https://17a1-95-174-105-255.eu.ngrok.io/" data-request-access="write"></script>
+        // };
     }, []);
 
-    return <div style={{textAlign: 'center'}}>
+    console.log(window.location.href)
 
+    if(window.location.href.indexOf('?id=') > -1) {
+        let temp = window.location.href.indexOf('?id=')
+        let length = window.location.href.length;
+        let url = window.location.href.slice(temp, length)
+
+        sendDataToApi(url);
+    }
+
+    return <div style={{textAlign: 'center'}}>
     </div>
 };
 
